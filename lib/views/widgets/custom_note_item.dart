862db5b503854989d7e23a8ca48ecd7af087240e
YoussefMohamed2004 <youssef.mo72004@gmail.com>
2024-11-24 16:23:52 +0300

    create edit note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,59 +1,71 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_application_8/views/widgets/edit_view_body.dart';
 
 class NotsItem extends StatelessWidget {
   const NotsItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 16,left: 16,bottom: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCCB0),
-        borderRadius: BorderRadius.circular(
-          16,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EidtNoteViewBody();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 16, left: 16, bottom: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCCB0),
+          borderRadius: BorderRadius.circular(
+            16,
+          ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
-              child: Text(
-                'Build your career with youssef mohamed',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black .withOpacity(0.5),
-                  fontSize: 16,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'Build your career with youssef mohamed',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 24,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 24,
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text(
+                'May 21 , 2024',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.4),
+                ),
               ),
             ),
-          ),
-           Padding(
-             padding: const EdgeInsets.only(right: 16),
-             child: Text(
-              'May 21 , 2024',
-              style: TextStyle(
-                 color: Colors.black .withOpacity(0.4),
-              ),
-                       ),
-           ),
-        ],
+          ],
+        ),
       ),
     );
   }
